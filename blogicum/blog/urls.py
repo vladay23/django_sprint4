95fe8491171dc028dc72b427a78b6e5691a21e8a
LovDay <98943140+vladay23@users.noreply.github.com>
2025-04-03 17:31:08 +0300

    Update

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -1,51 +1,38 @@
-from typing import List
-
-from django.urls import URLPattern, path
+from django.urls import include, path
 
 from . import views
 
 app_name = 'blog'
 
-urlpatterns: List[URLPattern] = [
-    path('', views.BlogIndexListView.as_view(), name='index'),
-    path(
-        'posts/<int:post_id>/', views.PostDetailView.as_view(),
-        name='post_detail'
-    ),
-    path(
-        'category/<slug:category_slug>/',
-        views.BlogCategoryListView.as_view(),
-        name='category_posts',
-    ),
-    path(
-        'profile/<str:username>/',
-        views.AuthorProfileListView.as_view(),
-        name='profile',
-    ),
-    path('posts/create/', views.PostCreateView.as_view(), name='create_post'),
-    path(
-        'posts/<int:post_id>/edit/',
-        views.PostUpdateView.as_view(),
-        name='edit_post',
-    ),
-    path(
-        'posts/<int:post_id>/delete/',
-        views.PostDeleteView.as_view(),
-        name='delete_post',
-    ),
-    path(
-        'posts/<int:post_id>/comment/',
-        views.CommentCreateView.as_view(),
-        name='add_comment',
-    ),
-    path(
-        'posts/<int:post_id>/edit_comment/<int:comment_id>/',
-        views.CommentUpdateView.as_view(),
-        name="edit_comment",
-    ),
-    path(
-        'posts/<int:post_id>/delete_comment/<int:comment_id>/',
-        views.CommentDeleteView.as_view(),
-        name='delete_comment',
-    ),
+post_urls = [
+    path('create/',
+         views.create_post, name='create_post'),
+    path('<int:post_id>/',
+         views.post_detail, name='post_detail'),
+    path('<int:post_id>/edit/',
+         views.edit_post, name='edit_post'),
+    path('<int:post_id>/delete/',
+         views.delete_post, name='delete_post'),
+    path('<int:post_id>/comment/',
+         views.add_comment, name='add_comment'),
+    path('<int:post_id>/edit_comment/<int:comment_id>/',
+         views.edit_comment, name='edit_comment'),
+    path('<int:post_id>/delete_comment/<int:comment_id>/',
+         views.delete_comment, name='delete_comment'),
+]
+
+profile_urls = [
+    path('edit/',
+         views.edit_profile, name='edit_profile'),
+    path('<slug:username>/',
+         views.profile, name='profile'),
+]
+
+urlpatterns = [
+    path('',
+         views.index, name='index'),
+    path('category/<slug:category_slug>/',
+         views.category_posts, name='category_posts'),
+    path('posts/', include(post_urls)),
+    path('profile/', include(profile_urls)),
 ]
