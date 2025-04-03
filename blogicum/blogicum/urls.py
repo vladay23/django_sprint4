1c2602fd10b87c2d4f538ea91a412e02d9c871bc
LovDay <98943140+vladay23@users.noreply.github.com>
2025-04-03 17:23:37 +0300

    Update

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,25 +1,26 @@
-from typing import List
-
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import URLPattern, include, path
+from django.views.generic.edit import CreateView
+from django.urls import include, path, reverse_lazy
 
-handler403 = 'pages.views.permission_denied'
+
 handler404 = 'pages.views.page_not_found'
-handler500 = 'pages.views.server_error'
+handler500 = 'pages.views.internal_server_error'
 
-urlpatterns: List[URLPattern] = [
-    path('pages/', include('pages.urls', namespace='pages')),
-    path('', include('users.urls', namespace='users')),
-    path('', include('blog.urls', namespace='blog')),
-    path('auth/', include('django.contrib.auth.urls')),
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
-]
-
-if settings.DEBUG:
-    import debug_toolbar
-
-    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('auth/', include('django.contrib.auth.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('blog:index'),
+        ),
+        name='registration',
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
