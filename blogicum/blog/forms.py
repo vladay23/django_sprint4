4b582e99f405fc2a962c6216b750e9152b5fd54c
LovDay <98943140+vladay23@users.noreply.github.com>
2025-04-03 17:30:19 +0300

    Update

--- blogicum/blog/forms.py
+++ blogicum/blog/forms.py
@@ -1,35 +1,24 @@
 from django import forms
-from django.utils import timezone
 
-from .models import Comment, Post
+from .models import Post, Comment, User
 
 
-class CreatePostForm(forms.ModelForm):
-    pub_date = forms.DateTimeField(
-        initial=timezone.now,
-        required=True,
-        widget=forms.DateTimeInput(
-            attrs={
-                'type': 'datetime-local',
-            },
-            format='%Y-%m-%dT%H:%M',
-        ),
-    )
+class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = (
-            'title',
-            'image',
-            'text',
-            'pub_date',
-            'location',
-            'category',
-            'is_published',
-        )
+        exclude = ('author',)
 
 
-class CreateCommentForm(forms.ModelForm):
+class CommentForm(forms.ModelForm):
+
     class Meta:
         model = Comment
-        fields = ("text",)
+        fields = ('text',)
+
+
+class UserForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
